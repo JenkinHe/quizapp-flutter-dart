51a47809b03461100b08d9ebc9fe388a0263ae47
Jenkibear <jenkin.he@students.mq.edu.au>
2024-04-06 21:42:02 +1100

    button creatin

---
+++ harrypotterquiz/lib/app_answer_button.dart
@@ -0,0 +1,32 @@
+
+
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget{
+  const AnswerButton ({required this.answerText,required this.onAnswerSelect,super.key});
+
+  final String answerText;
+  final void Function() onAnswerSelect;
+
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return ElevatedButton(
+      onPressed: onAnswerSelect,
+      style:ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 35,
+        ),
+        foregroundColor: const Color(0xffF2D3AC),
+        backgroundColor: const Color(0xff401201),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20)
+        )
+        ), 
+        child: Text(answerText)
+        );
+
+  }
+}
--- harrypotterquiz/lib/app_questions_screen.dart
+++ harrypotterquiz/lib/app_questions_screen.dart
@@ -1,24 +1,33 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:harrypotterquiz/app_answer_button.dart';
 
-class QuestionsScreen extends StatefulWidget{
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
 
- const  QuestionsScreen({super.key});
-
-@override
+  @override
   State<QuestionsScreen> createState() {
-    
     return _QuestionsScreen();
-    
   }
 }
 
-class _QuestionsScreen extends State<QuestionsScreen>{
+class _QuestionsScreen extends State<QuestionsScreen> {
   @override
-  Widget build( context) {
-    
-    return const Text("Quiz question screen");
-    
+  Widget build(context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("The Question"),
+          const SizedBox(
+            height: 30,
+          ),
+          AnswerButton(answerText: "Answer 1", onAnswerSelect: (){}),
+          AnswerButton(answerText: "Answer 2", onAnswerSelect: (){}),
+          AnswerButton(answerText: "Answer 3", onAnswerSelect: (){}),
+          AnswerButton(answerText: "Answer 4", onAnswerSelect: (){}),
+        ],
+      ),
+    );
   }
 }
